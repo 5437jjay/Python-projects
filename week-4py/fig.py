c74a3dcb3817cb885157cd3b6f3f4a0936cf8da1
5437jjay <nellaijayanth@gmail.com>
2026-06-20 21:26:55 +0530

    Add fig.py for text rendering with pyfiglet

---
+++ week-4py/fig.py
@@ -0,0 +1,31 @@
+from pyfiglet import Figlet
+import sys
+import random
+def main():
+    figlet = Figlet()
+
+    if len(sys.argv) == 1:
+        font = random.choice(figlet.getFonts())
+
+    elif len(sys.argv) == 3:
+        if sys.argv[1] not in ["-f", "--font"]:
+            sys.exit("Invalid usage")
+
+        if sys.argv[2] not in figlet.getFonts():
+            sys.exit("Invalid usage")
+
+        font = sys.argv[2]
+
+    else:
+        sys.exit("Invalid usage")
+
+    text = input("Input: ")
+
+    figlet.setFont(font=font)
+
+    print("Output:")
+    print(figlet.renderText(text))
+
+
+if __name__ == "__main__":
+    main()
